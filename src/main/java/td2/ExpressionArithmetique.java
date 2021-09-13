3016b57b9b0b56289891c7d4f4933d776dcbca8a
Goulois Lucas <lucas_goulois@ens.univ-artois.fr>
2021-09-13 11:11:48 +0200

    TD2

---
+++ src/main/java/td2/ExpressionArithmetique.java
@@ -0,0 +1,16 @@
+package td2;
+
+public abstract class ExpressionArithmetique {
+
+    public ExpressionArithmetique() {
+    }
+
+    public abstract double eval();
+
+    public abstract String toString();
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+}
